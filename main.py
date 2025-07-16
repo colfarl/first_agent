d186aea93aedc2e43f42be2e3a7a0cbe09d7b4a0
Colin Farley <colinfarley0423@gmail.com>
2025-07-16 15:49:41 -0400

    added LLM readable schema and tool for directory scan

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-import sys
+from config import SYSTEM_PROMPT
+from functions.get_files_info import schema_get_files_info
+
 
 # Get connect to google
 load_dotenv()
@@ -33,22 +35,40 @@ def main():
     args = parser.parse_args()
     user_prompt = args.input_str
     is_verbose = args.verbose
+    
+    # Configure LLM 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=SYSTEM_PROMPT
+        )
     )
 
     assert response.usage_metadata is not None, "Expected usage metadata"
-    print(response.text)
+        
     if is_verbose:
         print("User prompt:", user_prompt)
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-
+    
+    function_calls = response.function_calls
+    
+    if function_calls is not None and len(function_calls) > 0:
+        for function_call_part in function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     main()
