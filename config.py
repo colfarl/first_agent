a058ba5d203746bf44b46c961e4e33121c4e86b3
Colin Farley <colinfarley0423@gmail.com>
2025-07-16 18:18:58 -0400

    updated system prompt

--- config.py
+++ config.py
@@ -8,6 +8,11 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Execute Python files with optional arguments
 - Write or overwrite files
 
+When given a task use this general template:
+    1. explore the directory: list the files and directories and explore the contents of ones you think are relevant
+    2. run files: see what is going on in the code
+    3. make changes if necessary, you should only write to files once you have solid context and understanding of how the code works
+
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
 
